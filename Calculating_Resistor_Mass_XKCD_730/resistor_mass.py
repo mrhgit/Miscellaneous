ed8f134d5bfcff1c8d7c31ab77a9c7fc1c14e11d
Your Name <you@example.com>
2020-07-08 00:41:38 -0400

    Slight script cleanup

--- Calculating_Resistor_Mass_XKCD_730/resistor_mass.py
+++ Calculating_Resistor_Mass_XKCD_730/resistor_mass.py
@@ -41,8 +41,10 @@ mx = Matrix([
 
 [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
 [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1, 0, 1, 0, 0, 0, 0, 0,-1, 1, 0, 0],
+
 ])
 #01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 C
+#[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 # Print the Equations
 print("Equations Used")
@@ -66,10 +68,10 @@ for r in range(mx.rows):
 # Reduced Row Echelon Form
 rref = mx.rref()[0]
 print("\nCurrents (from Reduced Row Echelon Form)")
-for r in range(rref.rows-1):
-    print("i%d = %f" % (r,-1*rref[r,-1]))
+for r in range(rref.rows-2):
+    print("i%d = %f" % (r+1,-1*rref[r,-1]))
 
-# Print the Solution
-i = -1*(rref[0,-1] + rref[1,-1] + rref[2,-1] + rref[3,-1])
+# Find and Print the Solution
+i = -1*(rref[0,-1] + rref[1,-1] + rref[2,-1] + rref[3,-1]) # Multiply by -1 because these solutions are of the form (1)*i1 + (-i1val)*1 = 0
 print("\nSolution:\nI = i1 + i2 + i3 + i4 = %f, R = 1/I = %f" % (i,1./i))
 
