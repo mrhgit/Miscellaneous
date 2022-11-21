8617c55e22fd361029371cdad51fd9a2c50e7966
Mark H <mrhgit@users.noreply.github.com>
2022-11-20 21:05:55 -0500

    Update runremote.sh

--- RaspberryPi_InfraredRemoteControl/bin/runremote.sh
+++ RaspberryPi_InfraredRemoteControl/bin/runremote.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# sudo echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-sudo /home/pi/Desktop/ir/bin/remotecontroller.exe $1
-# sudo echo ondemand > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-
-
-## sudo sh -c "echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
-## sudo /home/pi/Desktop/ir/bin/remotecontroller.exe $1
-## sudo sh -c "echo ondemand > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
+sudo $( dirname -- "0"; )/remotecontroller.exe $1
